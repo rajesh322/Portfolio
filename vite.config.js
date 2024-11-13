80bd8243ec6ede46e63e8d867553c0d9ccabd221
carz1899 <carscommunity18@gmail.com>
2024-11-13 18:56:27 +0530

    Add homepage and deployment scripts; configure base path for portfolio

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,8 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import path from 'path'
 
-// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  base: '/Portfolio/',  // Note the forward slashes before and after
 })
